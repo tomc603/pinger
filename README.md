c6ccd11abc4f38d80f5416f29c33b421a69778dd
Tom Cameron <tomc603@gmail.com>
2018-05-16 13:35:33 -0400

    Detail DB in README and build better ping
    Break ping functionality out of main() and into its own function
    Add more database details to the README file

--- README.md
+++ README.md
@@ -4,6 +4,30 @@ Pinger pings hosts and stores results in a database
 ---
 # Database
 
-Hosts are stored in a table along with parameters such as timeout, data size, and protocol (UDP, TCP, ICMP, UDP6, TCP6, ICMP6).
+## Sources
+A source is a host running `pinger`. Each host receives its own ID in the **sources** table, which is used in part to
+create an identifier value in packets sent to a **destination**.
 
-Results are stored in a table, linked to the PK of a host. Stored results include responding address, response code, and whether the data received matches the data sent.
+id | hostname | source id
+--- | --- | ---
+1 | source1.example.net | 24
+2 | source2.example.net | 28
+
+## Destinations
+Destinations are addresses stored in a table along with the parameters timeout, ttl/hlim, data size, 
+and protocol (UDP, TCP, ICMP, UDP6, TCP6, ICMP6).
+
+id | hostname | protocol | timeout | ttl | data size
+--- | -------- | -------- | ------- | --- | --------
+1 | host1.example.com | udp6 | 500 | | 128
+2 | host2.example.net | icmp | 250 | 15 | 64
+
+## Results
+A Result is a response to a probe sent to a **destination**. Responses are stored in a table, linked to the PK of a
+**destination**, and the PK of a **source**. A Result includes responding address, response type, response code, and
+whether the data received matches the data sent.
+
+id | source | destination | responder | response type | response code | data match
+--- | --- | --- | --- | --- | --- | ---
+121 | 2 | 2 | 192.0.2.4 | 0 | 0 | true
+212 | 1 | 1 | 2001:0DB8:dead:beef:face::4 | 0 | 0 | true
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"net"
 	"os"
 
 	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 	"golang.org/x/net/ipv6"
 )
 
@@ -14,53 +16,127 @@ const (
 	ProtoICMPv6 = 58
 )
 
+func ping(conn *icmp.PacketConn, addr string) {
+	echoRequestBody := icmp.Echo{
+		ID: os.Getpid() & 0xffff,
+		Seq: 1,
+		Data: []byte("ICMPTeST"),
+	}
+
+	v6Addr, err := net.ResolveIPAddr("ip6", addr)
+	if err != nil {
+		switch err.(type) {
+		case *net.DNSError:
+			e := err.(*net.DNSError)
+			if e.IsTimeout {
+				log.Fatalf("DNS Timeout: %#v", e.Name)
+			} else if e.IsTemporary {
+				log.Fatalf("DNS Temporary Failure: %#v", e)
+			} else {
+				log.Fatalf("DNS Error: %#v", e)
+			}
+		case *net.AddrError:
+			log.Printf("No v6 Address: '%s'. %s", err.(*net.AddrError).Addr, err.(*net.AddrError).Err)
+		default:
+			log.Fatalf("Unexpected error: %#v", err)
+		}
+	} else {
+		log.Printf("v6 Address: %v", v6Addr)
+		echoRequestMessage := icmp.Message{
+			Type: ipv6.ICMPTypeEchoRequest,
+			Code: 0,
+			Body: &echoRequestBody,
+		}
+
+		echoRequest, err := echoRequestMessage.Marshal(nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if _, err := conn.WriteTo(echoRequest, &net.UDPAddr{IP: v6Addr.IP}); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	v4Addr, err := net.ResolveIPAddr("ip4", addr)
+	if err != nil {
+		switch err.(type) {
+		case *net.DNSError:
+			e := err.(*net.DNSError)
+			if e.IsTimeout {
+				log.Fatalf("DNS Timeout: %#v", e.Name)
+			} else if e.IsTemporary {
+				log.Fatalf("DNS Temporary Failure: %#v", e)
+			} else {
+				log.Fatalf("DNS Error: %#v", e)
+			}
+		case *net.AddrError:
+			log.Printf("No v6 Address: '%s'. %s", err.(*net.AddrError).Addr, err.(*net.AddrError).Err)
+		default:
+			log.Fatalf("Unexpected error: %#v", err)
+		}
+	} else {
+		log.Printf("v4 Address: %v", v4Addr)
+		echoRequestMessage := icmp.Message{
+			Type: ipv4.ICMPTypeEcho,
+			Code: 0,
+			Body: &echoRequestBody,
+		}
+
+		echoRequest, err := echoRequestMessage.Marshal(nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if _, err := conn.WriteTo(echoRequest, &net.UDPAddr{IP: v4Addr.IP}); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
 func main() {
-	c, err := icmp.ListenPacket("udp6", "::")
+	conn, err := icmp.ListenPacket("ip6:ipv6-icmp", "::")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer c.Close()
+	defer conn.Close()
 
-	wmsg := icmp.Message{
-		Type: ipv6.ICMPTypeEchoRequest,
-		Code: 0,
-		Body: &icmp.Echo{
-			ID: os.Getpid() & 0xffff,
-			Seq: 1,
-			Data: []byte("ICMPTeST"),
-		},
-	}
+	ping(conn,"www.reddit.com.")
 
-	mmsg, err := wmsg.Marshal(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//destinationV6Addr, err := net.ResolveIPAddr("udp6", "google-public-dns-a.google.com.")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
-	if _, err := c.WriteTo(mmsg, &net.UDPAddr{IP: net.ParseIP("2001:4860:4860::8888")}); err != nil {
-		log.Fatal(err)
-	}
+	//if _, err := conn.WriteTo(echoRequest, &net.UDPAddr{IP: destinationV6Addr.IP}); err != nil {
+	//	log.Fatal(err)
+	//}
 
 	for {
-		rbuf := make([]byte, 1500)
-		n, peer, err := c.ReadFrom(rbuf)
+		receiveBuffer := make([]byte, 1500)
+		n, peer, err := conn.ReadFrom(receiveBuffer)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		rmsg, err := icmp.ParseMessage(ProtoICMPv6, rbuf[:n])
+		receiveMessage, err := icmp.ParseMessage(ProtoICMPv6, receiveBuffer[:n])
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		switch rmsg.Type {
+		switch receiveMessage.Type {
 		case ipv6.ICMPTypeEchoReply:
 			// TODO: Convert the message into an icmp.Echo to parse out the data.
-			//recho := rmsg.Body.(icmp.Echo)
-			log.Printf("Reflection from %v\n %#v", peer, rmsg)
+			echoReply := receiveMessage.Body.(*icmp.Echo)
+			log.Printf("ID %v, Seq: %v, From: %v\nData: %v\n",
+				echoReply.ID,
+				echoReply.Seq,
+				peer,
+				bytes.Compare(echoReply.Data, echoRequestBody.Data) == 0)
 		case ipv6.ICMPTypeEchoRequest:
 			continue
 		default:
-			log.Printf("Got %v from %v.\n%#v", rmsg.Type, peer, rmsg)
+			log.Printf("Got %v from %v.\n%#v", receiveMessage.Type, peer, receiveMessage)
 		}
 	}
 }
